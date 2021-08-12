5eceab39e778e5c993f96e497c81686019a050e4
youkharoufi <87651323+youkharoufi@users.noreply.github.com>
2021-08-12 05:31:08 -0700

    Add files via upload

---
+++ Pages/Menu.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from './Button';
+import {useHistory} from 'react-router-dom';
+import styles from '../CSS/Menu.module.css'
+import Card from './Card';
+
+
+
+const Menu=(props)=>{
+
+    let history=useHistory();
+
+    const state1=()=>{
+        history.push('/Account')
+    }
+
+    const state2=()=>{
+        history.push('/Depot')
+    }
+
+    const state3=()=>{
+        history.push('/Withdraw')
+    }
+
+    const preced=()=>{
+        history.push('/')
+    }
+
+    
+    
+
+
+    return(
+        <Card className={styles.menu}>
+            
+                <h2>Bienvenue !</h2>
+            
+            <h2>Que Voulez-vous Faire ?</h2>
+                <br></br>
+                <div onClick={state1}>Voir vos comptes ?</div>
+                <br></br>
+                <div onClick={state2}>Effectuer un Dépôt ?</div>
+                <br></br>
+                <div onClick={state3}>Effectuer un Retrait ?</div>
+                <br></br>
+                <Button onClick={preced}>Deconnexion</Button>
+            
+        </Card>
+    )
+}
+
+export default Menu;
